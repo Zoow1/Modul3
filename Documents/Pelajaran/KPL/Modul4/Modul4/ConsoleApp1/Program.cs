55c3ff7e3295b8b3a1564f2bd8422aeef396b1b8
Zoow1 <127001727+Zoow1@users.noreply.github.com>
2023-03-11 00:18:02 +0700

    STATE-BASED

--- Documents/Pelajaran/KPL/Modul4/Modul4/ConsoleApp1/Program.cs
+++ Documents/Pelajaran/KPL/Modul4/Modul4/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Modul4.program.doormachine;
 
 namespace Modul4
 {
@@ -22,6 +23,70 @@ namespace Modul4
             Maleer,
             Samoja
         };
+        public enum pintu
+        {
+            terkunci,
+            terbuka
+        }
+        public enum Trigger
+        {
+            pintu_buka,
+            pintu_kunci
+        }
+        
+
+        public class doormachine
+        {
+            public pintu currentstate = pintu.terkunci;
+            public class Transisi
+            {
+                public pintu stateawal;
+                public pintu stateakhir;
+                public Trigger trigger;
+                public Transisi(pintu stateawal, pintu stateakhir, Trigger trigger)
+                {
+                    this.stateawal = stateawal;
+                    this.stateakhir = stateakhir;
+                    this.trigger = trigger;
+                }
+            }
+
+            Transisi[] transisii =
+            {
+                new Transisi(pintu.terkunci, pintu.terbuka, Trigger.pintu_buka),
+                new Transisi(pintu.terbuka, pintu.terkunci, Trigger.pintu_kunci),
+                new Transisi(pintu.terbuka, pintu.terbuka, Trigger.pintu_buka),
+                new Transisi(pintu.terkunci, pintu.terkunci, Trigger.pintu_kunci)
+            };
+
+            private pintu getstateberikutnya(pintu stateawal, Trigger trigger)
+            {
+                pintu stateakhir = stateawal;
+                for(int i = 0; i < transisii.Length; i++)
+                {
+                    Transisi perubahan = transisii[i];
+                    if(stateawal == perubahan.stateawal && trigger == perubahan.trigger)
+                    {
+                        stateakhir = perubahan.stateakhir;
+                    }
+                }
+                return stateakhir;
+                 
+            }
+            public void trigeraktif(Trigger trigger)
+            {
+                currentstate = getstateberikutnya(currentstate, trigger);
+                Console.WriteLine("State sekarang adalah " + currentstate);
+                if(currentstate == pintu.terkunci)
+                {
+                    Console.WriteLine("pintu sedang terkunci");
+                }else if(currentstate == pintu.terbuka)
+                {
+                    Console.WriteLine("pintu sedang terbuka");
+                }
+            }
+        }
+
         public class getkodepos
         {
             //public static int[] kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 4027, 40273 };
@@ -34,6 +99,16 @@ namespace Modul4
         public static void Main(string[] args)
         {
             //getkodepos KODEPOS = new getkodepos();
+            doormachine Pintu = new doormachine();
+            Pintu.trigeraktif(Trigger.pintu_kunci);
+            Pintu.trigeraktif(Trigger.pintu_buka);
+            Pintu.trigeraktif(Trigger.pintu_kunci);
+            Pintu.trigeraktif(Trigger.pintu_kunci);
+            Pintu.trigeraktif(Trigger.pintu_buka);
+            Pintu.trigeraktif(Trigger.pintu_buka);
+
+            Console.WriteLine("----------------------------");
+
             datakota kota = datakota.Batununggal;
             int kode = getkodepos.getkode(kota);
             Console.WriteLine("kelurahan : " + kota + "\nkodepos : " + kode);
