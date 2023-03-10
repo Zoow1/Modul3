73d13b5140fee7890a4e33d45781c1aaaee8fa84
Zoow1 <127001727+Zoow1@users.noreply.github.com>
2023-03-10 22:42:06 +0700

    menambahkan table driven

---
+++ Documents/Pelajaran/KPL/Modul4/Modul4/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4
+{
+    internal class program
+    {
+        public enum datakota
+        {
+            Batununggal,
+            Kujangsari,
+            Mengger,
+            Wates,
+            Cijaura,
+            Jatisari,
+            Margasari,
+            Sekejati,
+            Kebonwaru,
+            Maleer,
+            Samoja
+        };
+        public class getkodepos
+        {
+            //public static int[] kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 4027, 40273 };
+            public static int getkode(datakota kota)
+            {
+                int[] kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 4027, 40273 };
+                return kodepos[(int)kota];
+            }
+        }
+        public static void Main(string[] args)
+        {
+            //getkodepos KODEPOS = new getkodepos();
+            datakota kota = datakota.Batununggal;
+            int kode = getkodepos.getkode(kota);
+            Console.WriteLine("kelurahan : " + kota + "\nkodepos : " + kode);
+        }
+    }
+}
